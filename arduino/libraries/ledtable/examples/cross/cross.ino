275f0467ba093905c456cc79adbf968d4899763f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-19 02:13:41 +0100

    made method used

--- arduino/libraries/ledtable/examples/cross/cross.ino
+++ arduino/libraries/ledtable/examples/cross/cross.ino
@@ -7,7 +7,7 @@ LEDTable ledtable = LEDTable(6, 12, 12, PIXELORDER(snake));
 void setup() {
   ledtable.begin();
   ledtable.fill(color_black);
-  for (int i = 0; i < ledtable.height; i++) {
+  for (int i = 0; i < ledtable.height(); i++) {
     ledtable.fill(i, i, color_blue);
     ledtable.fill(ledtable.height - i - 1, i, color_green);
   }
