3db0a928e1f58526a6628d74c4a70f7bb619f870
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-24 18:20:58 +0100

    added more intuitive input recognition

--- arduino/libraries/ledtable/examples/tron/tron.ino
+++ arduino/libraries/ledtable/examples/tron/tron.ino
@@ -1,9 +1,14 @@
 #include <ledtable.h>
 
+/* These constants can change the game */
 #define NUMBER_OF_PLAYERS 4
-#define MAXIMUM_NUMBER_OF_PLAYERS 8
 #define STEPS_TO_HOLE 5
 #define BACKGROUND_COLOR color_black
+#define TIME_BETWEEN_ROUNDS_MILLIS 300
+
+/* These are private constants */
+#define MAXIMUM_NUMBER_OF_PLAYERS 8
+#define EFFECT_COLORS 5
 
 LEDTable ledtable = LEDTable(2, 20, 15, PIXELORDER<flip_xy, snake>);
 
@@ -14,8 +19,60 @@ const int playerPins[MAXIMUM_NUMBER_OF_PLAYERS][PINS_PER_PLAYER] = {{3,4},{5,6},
 const Color colors[MAXIMUM_NUMBER_OF_PLAYERS] = {color_red, color_green, color_yellow, color_violet, 
                                                  color_orange, color_darkgreen, color_darkgoldenrod, color_white};
 
+class Effect
+{
+private:
+  const Color colors[EFFECT_COLORS] = {color_black, color_black, color_black, color_black, color_black};
+  int x;
+  int y;
+  uint8_t color_index;
+  bool activated; 
+public:
+  Effect()
+  {
+    activated = false;
+    color_index = 0;
+  }
+
+  void activate()
+  {
+    for (int i = 10; i; i--)
+    {
+      x = random(ledtable.minX(), ledtable.maxX() + 1);
+      y = random(ledtable.minY(), ledtable.maxY() + 1);
+      activated = ledtable.at(x, y) == BACKGROUND_COLOR;
+      if (activated)
+      {
+        break;
+      }
+    }
+  }
+
+  void update()
+  {
+    if (activated)
+    {
+      paint();
+      collide();
+    }
+  }
+
+  void paint()
+  {
+    color_index = (color_index + 1) % EFFECT_COLORS;
+    ledtable.fill(x, y, colors[color_index]);
+  }
+
+  void collide()
+  {
+    
+  }
+};
+
+
 enum PlaterState {MOVING, EXPLODING, END_OF_EXPLOSION, DEAD};
 enum PlayerDirection {DOWN = 0, LEFT = 1, UP = 2, RIGHT = 3, STANDING};
+enum ButtonState {CATCHING_INPUT, WAITING_FOR_PLAYER_TO_SWITCH_BACK};
 
 class Player
 {
@@ -25,9 +82,11 @@ private:
   int last_x;
   int last_y;
   uint8_t index;
-  PlayerDirection direction; /* 0 = down, 1 = left, 2 = up, 3  = right, 4 = standing */
+  PlayerDirection direction;
+  PlayerDirection newDirection;
   unsigned int walked;
   PlaterState state;
+  ButtonState inputState;
 public:
   Player()
   {
@@ -48,6 +107,8 @@ public:
     last_y = y;
     walked = 0;
     state = MOVING;
+    newDirection = direction;
+    inputState = CATCHING_INPUT;
     pinMode(rightPin(), INPUT);
     digitalWrite(rightPin(), HIGH);
     pinMode(leftPin(), INPUT);
@@ -67,6 +128,16 @@ public:
   const int rightPin()
   {
     return playerPins[index][1];
+  }
+
+  bool leftPressed()
+  {
+    return digitalRead(leftPin()) == LOW;
+  }
+
+  bool rightPressed()
+  {
+    return digitalRead(rightPin()) == LOW;
   }
 
   void setup()
@@ -105,6 +176,8 @@ public:
   {
     ledtable.fill(x - 1, y - 1, x + 2, y + 2, BACKGROUND_COLOR);
     state = DEAD;
+    x = -100;
+    y = -100;
   }
 
   void explode() {
@@ -122,13 +195,10 @@ public:
 
   void react()
   {
-    if (digitalRead(leftPin()) == LOW)
+    if (inputState == CATCHING_INPUT)
     {
-      direction = PlayerDirection((direction + 3) % 4);
-    }
-    if (digitalRead(rightPin()) == LOW)
-    {
-      direction = PlayerDirection((direction + 1) % 4);
+      direction = newDirection;
+      inputState = WAITING_FOR_PLAYER_TO_SWITCH_BACK;
     }
   }
 
@@ -137,12 +207,26 @@ public:
     ledtable.fill(last_x, last_y, transparent(BACKGROUND_COLOR, walked % STEPS_TO_HOLE ? 150 : 0));
     ledtable.fill(x, y, transparent(color(), 200));
   }
-};
 
-class Effect
-{
-  
-}
+  void catchInput()
+  {
+    if (inputState == WAITING_FOR_PLAYER_TO_SWITCH_BACK && !rightPressed() && !leftPressed())
+    {
+      inputState = CATCHING_INPUT;
+    }
+    if (inputState == CATCHING_INPUT)
+    {
+      if (leftPressed())
+      {
+        newDirection = PlayerDirection((direction + 3) % 4);
+      }
+      if (rightPressed())
+      {
+        newDirection = PlayerDirection((direction + 1) % 4);
+      }
+    }
+  }
+};
 
 
 Player players[8] = { Player(), Player(), Player(), Player(), Player(), Player(), Player(), Player() };
@@ -163,18 +247,50 @@ void setupPlayers()
   }
 }
 
+void catchInput()
+{
+  for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
+  {
+    players[i].catchInput();
+  }
+}
+
+int frame;
+
 void setup()
 {
+  frame = 0;
   ledtable.begin();
   ledtable.fill(BACKGROUND_COLOR);
+  //ledtable.brightness(50);
   setupPlayers();
+}
+
+void wait()
+{
+  int now = millis();
+  int stop = millis() + TIME_BETWEEN_ROUNDS_MILLIS;
+  int offset;
+  if (stop < now) 
+  {
+    offset = - 2 * TIME_BETWEEN_ROUNDS_MILLIS;
+    now -= offset;
+    stop -= offset;
+  } else  {
+    offset = 0;
+  }
+  for (int i = millis(); i + offset < stop; i = millis())
+  {
+    catchInput();
+  }
 }
 
 void loop()
 {
   updatePlayers();
   ledtable.show();
-  delay(300);
+  wait();
+  frame++;
 }
 
 
