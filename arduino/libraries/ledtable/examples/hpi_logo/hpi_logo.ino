1340f89b8a6f3cd6b33361269cf32cc6383a2dce
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-04-27 14:48:02 +0200

    addd printing pixel order

--- arduino/libraries/ledtable/examples/hpi_logo/hpi_logo.ino
+++ arduino/libraries/ledtable/examples/hpi_logo/hpi_logo.ino
@@ -5,25 +5,27 @@
 #include <Adafruit_NeoPixel.h>
 #include <ledtable.h>
 
-LEDTable ledtable = LEDTable(6, 20, 15, PIXELORDER<flip_xy, rotate_180, snake>);
+LEDTable ledtable = LEDTable(6, 20, 15, PIXELORDER<rotate_180, snake>);
 
 //    1   1   1 1   1
 //    1 1 1   1 1   1
 //    1   1   1     1
-const uint32_t HPIbits[3] = {0b10101101, 
-                             0b11101101, 
-                             0b10101001};
-Stamp HPI = Stamp(HPIbits, 8, 3);
+Stamp HPI = Stamp(0b10101101, 
+                  0b11101101, 
+                  0b10101001);
 
 void setup() {
+  Serial.begin(9600);
   ledtable.begin();
-  ledtable.brightness(20);
+  ledtable.brightness(50);
   ledtable.fill(color_black);
   ledtable.fill(0, 12, 10, 2, RGB(177, 7, 10));
   ledtable.fill(2, 10, 12, 0, RGB(247, 169, 0));
   ledtable.fill(2, 10, 10, 2, RGB(222, 98, 7));
   HPI.stamp(&ledtable, 2, 3);
   ledtable.show();
+  ledtable.printToSerial();
+  ledtable.printPixelOrderToSerial();
 }
 
 void loop() {
